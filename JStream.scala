55ec56d3f2b0322183a63843c8f66d6597b7937d
Cereopsis <Cereopsis@users.noreply.github.com>
2018-04-04 09:14:44 +0100

    Implement various in terms of foldRight

--- JStream.scala
+++ JStream.scala
@@ -1,7 +1,7 @@
 sealed trait JStream[+A] { self =>
 
   def toList: List[A] = foldLeft(List[A]())(_ :: _).reverse
-  
+
   def take(n: Int): JStream[A] = self match {
     case Cons(a, t) if n > 0 => JStream.cons(a(), t().take(n - 1))
     case _ => JStream.empty
@@ -12,21 +12,23 @@ sealed trait JStream[+A] { self =>
     case _ => self
   }
 
-  def takeWhile(p: A => Boolean): JStream[A] = self match {
-    case Cons(a, t) if p(a()) => JStream.cons(a(), t().takeWhile(p))
-    case _ => JStream.empty
-  }
-  
+  def takeWhile(p: A => Boolean): JStream[A] =
+    foldRight(JStream.empty: JStream[A]){ (a,b) => 
+      if (p(a)) JStream.cons(a, b)
+      else b
+    }
+
   def dropWhile(p: A => Boolean): JStream[A] = self match {
     case Cons(a, t) if p(a()) => t().dropWhile(p)
     case _ => self
   }
 
-  def exists(p: A => Boolean): Boolean = self match {
-    case Cons(a, t) => p(a()) || t().exists(p)
-    case _ => false
-  }
-  
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a,b) => p(a) || b)
+
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a,b) => p(a) && b)
+
   def foldLeft[B](z: B)(f: (A, => B) => B): B = self match {
     case Cons(a, t) => t().foldLeft(f(a(), z))(f)
     case _ => z
@@ -37,6 +39,33 @@ sealed trait JStream[+A] { self =>
     case _ => z
   }
 
+  def headOption: Option[A] =
+    foldRight(None: Option[A]){ (a,b) =>
+      if (a == Empty) b
+      else Some(a)
+    }
+
+  def map[B](f: A => B): JStream[B] =
+    foldRight(JStream.empty: JStream[B]){ (a, b) =>
+      JStream.cons(f(a), b)
+    }
+
+  def filter(p: A => Boolean): JStream[A] =
+    foldRight(JStream.empty: JStream[A]){ (a,b) =>
+      if (p(a)) JStream.cons(a, b)
+      else b
+    }
+
+  def append[AA >: A](a: => AA): JStream[AA] =
+    foldRight(JStream.cons(a, JStream.empty)){ (aa,b) => 
+      JStream.cons(aa, b)
+    }
+
+  // TODO: fix flatMap
+  // def flatMap[B](f: A => JStream[B]): JStream[B] =
+  //   foldRight(JStream.empty: JStream[B]){ (a,b) =>
+  //     f(a)
+  //   }
 }
 
 final case object Empty extends JStream[Nothing]
